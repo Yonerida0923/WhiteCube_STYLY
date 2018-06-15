9e2cf9827a72d33279f9243c76124e91fe3538b4
Yonerida0923 <kyohei.yonemoto@gmail.com>
2018-06-15 17:05:41 +0900

    .gitignoreに_Outputフォルダ(ビルド)が追加されないように設定しました。
    ノブがローカルで作成していたプロジェクトを上げます。

---
+++ WhiteCubeProject_forStyly/Assets/AutoRotateClothes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotateClothes : MonoBehaviour {
+
+    public float rotateSpeed = 25f;
+
+	void Update ()
+    {
+        float angle = Time.deltaTime * rotateSpeed;
+        transform.Rotate(0f, angle, 0f);
+	}
+}
---
+++ WhiteCubeProject_forStyly/Assets/ResetScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+
+public class ResetScene : NetworkBehaviour
+{
+    [SerializeField]
+    private string sceneToTransferTo;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Entered ResetScene");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RpcResetScene();
+        }
+    }
+
+    /**
+     * シーン移動する：今回はロビーシーンに戻る
+     */
+    [ClientRpc]
+    private void RpcResetScene()
+    {
+        Debug.Log("Will reset scene to Lobby");
+        NetworkManager.singleton.ServerChangeScene(sceneToTransferTo);      // from https://answers.unity.com/questions/1391780/how-to-use-networkmanagerserverchangescene.html
+    }
+}
